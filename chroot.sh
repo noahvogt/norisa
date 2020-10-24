994c6ab60ea8fb6eed8a1326d9318aa19f61c493
Noah <noah.vogt@protonmail.ch>
2020-10-24 12:46:36 +0000

    safing chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -5,21 +5,28 @@
 # do this if you exactly understand EVERY SINGLE
 # LINE of this bash script. You'll thank me later.
 
+read -p "Press any key to continue"
 passwd
+read -p "Press any key to continue"
 
 TZuser=$(cat tzfinal.tmp)
 
 ln -sf /usr/share/zoneinfo/$TZuser /etc/localtime
+read -p "Press any key to continue"
 
 hwclock --systohc
+read -p "Press any key to continue"
 
 echo "LANG=en_GB.UTF-8" >> /etc/locale.conf
 echo "en_GB.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
+read -p "Press any key to continue"
 
 pacman --noconfirm --needed -S networkmanager
 systemctl enable NetworkManager
 systemctl start NetworkManager
+read -p "Press any key to continue"
 
 DRIVE=$(cat drive)
 pacman --noconfirm --needed -S grub && grub-install --target=i386-pc /dev/${DRIVE} && grub-mkconfig -o /boot/grub/grub.cfg
+read -p "Press any key to continue"
