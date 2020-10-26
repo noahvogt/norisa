6ec7274dc982e9e79c599bd6a7d7dcb6916b784e
Noah Vogt <noah.vogt@protonmail.ch>
2020-10-26 20:06:41 +0100

    script is now working

--- norisa.sh
+++ norisa.sh
@@ -6,51 +6,57 @@ pacman -Sy --noconfirm git vim ||  { printf "Error at script start:\n\nAre you s
 
 # make new user
 sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
-read -pr "What is your desired username? " username
+read -p "What is your desired username? " username
 useradd -m -g users -G wheel "$username"
 passwd "$username"
 cd /home/"$username" || exit
 
 # setting up build environment
-mkdir -p ~/.build && cd ~/.build || exit
+sudo -u "$username" mkdir -p /home/"$username"/.build && cd .. && chown -R "$username":wheel "$username"/.build/
+pwd
+ls "$username"
 
 # install aur helper (pacaur)
-cd /tmp || exit
+cd /home/"$username"/.build || exit
 pacman -S --noconfirm expac jq
 rm -rf /tmp/pacaur*
 curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz &&
-sudo -u "$username" tar -xvf yay.tar.gz
+tar xvf yay.tar.gz
+cd .. && chown -R "$username":wheel .build/ && cd .build
 cd yay || exit
 sudo -u "$username" makepkg --noconfirm -si
 
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xwininfo xorg-xprop xorg-xbacklight xorg-xdpyinfo xorg-xsetroot 
+sudo -u "$username" yay -S --noconfirm libxft-bgra-git
+
 # clone dotfiles repo
-cd /tmp || exit
+cd /home/"$username"/.build || exit
 git clone https://github.com/noahvogt/dotfiles.git
-cp /tmp/dotfiles/.* /home/"$username" /root
+cp dotfiles/.* /home/"$username" /root
 
 # shellcheck source=/dev/null
 source ~/.bashrc
 
 # build dwm
-cd /tmp || exit
+cd /home/"$username"/.build || exit
 git clone https://github.com/noahvogt/dwm.git
-cd /dwm || exit
+cd dwm || exit
 make clean install
 
 # build st
-cd /tmp || exit
+cd /home/"$username"/.build || exit
 git clone https://github.com/noahvogt/st.git
-cd /st || exit
+cd st || exit
 make clean install
 
 # build dwmblocks
-cd /tmp || exit
+cd /home/"$username"/.build || exit
 git clone https://github.com/noahvogt/dwmblocks.git
-cd /dwmblocks || exit
+cd dwmblocks || exit
 make clean install
 
 # download packages from the official repo
-pacman -S xorg-server xorg-xinit xorg-xwininfo xorg-xprop xorg-xbacklight xorg-xdpyinfo xorg-xsetroot picom xbindkeys jdk-openjdk dmenu geogebra shellcheck vim firefox syncthing ranger xournalpp ffmpeg obs-studio sxiv arandr man-db brightnessctl unzip unrar python mupdf mediainfo highlight pulseaudio-alsa pulsemixer pamixer  ttf-linux-libertine calcurse xclip noto-fonts-emoji imagemagick thunderbird gimp xorg-setxkbmap wavemon cmus texlive-most
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xwininfo xorg-xprop xorg-xbacklight xorg-xdpyinfo xorg-xsetroot picom xbindkeys jdk-openjdk dmenu geogebra shellcheck vim firefox syncthing ranger xournalpp ffmpeg obs-studio sxiv arandr man-db brightnessctl unzip unrar python mupdf mediainfo highlight pulseaudio-alsa pulsemixer pamixer  ttf-linux-libertine calcurse xclip noto-fonts-emoji imagemagick thunderbird gimp xorg-setxkbmap wavemon cmus texlive-most
 
 # install aur packages
-sudo -u "$username" yay -S betterlockscreen simple-mtpfs-git tibasicc-git xflux
+sudo -u "$username" yay -S --noconfirm betterlockscreen simple-mtpfs-git tibasicc-git xflux
