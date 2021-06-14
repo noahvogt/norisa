f0a387d0f3312c93a5f7f741753bea222ab51bc7
Noah Vogt <noah@noahvogt.com>
2021-06-14 09:46:27 +0200

    important updates

--- norisa.sh
+++ norisa.sh
@@ -13,63 +13,60 @@ passwd "$username"
 cd /home/"$username" || exit
 
 # setting up build environment
-sudo -u "$username" mkdir -p /home/"$username"/.build && cd .. && chown -R "$username":wheel "$username"/.build/
+sudo -u "$username" mkdir -p /home/"$username"/.local/src && cd .. && chown -R "$username":wheel "$username"/.local/src/
 pwd
 ls "$username"
 
-# install aur helper (pacaur)
-cd /home/"$username"/.build || exit
-pacman -S --noconfirm expac jq
-rm -rf /tmp/pacaur*
-curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/yay.tar.gz &&
-tar xvf yay.tar.gz
-cd .. && chown -R "$username":wheel .build/ && cd .build || exit
-cd yay || exit
+# install aur helper (paru)
+cd /home/"$username"/.local/src || exit
+pacman -S --noconfirm asp bat devtools
+curl -sO https://aur.archlinux.org/cgit/aur.git/snapshot/paru.tar.gz &&
+tar xvf paru.tar.gz
+cd .. && chown -R "$username":wheel /home/"$username"/.local/src/ && cd .local/src || exit
+cd paru || exit
 sudo -u "$username" makepkg --noconfirm -si
 
 pacman -S --noconfirm xorg-server xorg-xinit xorg-xwininfo xorg-xprop xorg-xbacklight xorg-xdpyinfo xorg-xsetroot 
-sudo -u "$username" yay -S --noconfirm libxft-bgra-git
+sudo -u "$username" paru -S --noconfirm libxft-bgra-git
 
 # clone dotfiles repo
-cd /home/"$username"/.build || exit
-git clone https://github.com/noahvogt/dotfiles.git
+cd /home/"$username"/.local/src || exit
+git clone git@github.com:noahvogt/dotfiles.git
 cp -f dotfiles/.* /home/"$username" /root
 
-# shellcheck source=/dev/null
-source ~/.bashrc
-
 # build dwm
-cd /home/"$username"/.build || exit
-git clone https://github.com/noahvogt/dwm.git
+cd /home/"$username"/.local/src || exit
+git clone git@github.com:noahvogt/dwm.git
 cd dwm || exit
 make clean install
 
 # build st
-cd /home/"$username"/.build || exit
-git clone https://github.com/noahvogt/st.git
+cd /home/"$username"/.local/src || exit
+git clone git@github.com:noahvogt/st.git
 cd st || exit
 make clean install
 
 # build dwmblocks
-cd /home/"$username"/.build || exit
-git clone https://github.com/noahvogt/dwmblocks.git
+cd /home/"$username"/.local/src || exit
+git clone git@github.com:noahvogt/dwmblocks.git
 cd dwmblocks || exit
 make clean install
 
 # build dmenu
-cd /home/"$username"/.build || exit
-git clone https://github.com/noahvogt/dmenu.git
+cd /home/"$username"/.local/src || exit
+git clone git@github.com:noahvogt/dmenu.git
 cd dmenu || exit
 make clean install
 
-# make user to owner of ~/.build
-chown -R "$username":wheel home/"$username"/.build
+# make user to owner of ~/ and /mnt/
+chown -R "$username":wheel /home/"$username"/
+chown -R "$username":wheel /mnt/
 
 # download packages from the official repo
-pacman -S --noconfirm xorg-server xorg-xinit xorg-xwininfo xorg-xprop xorg-xbacklight xorg-xdpyinfo xorg-xsetroot picom xbindkeys jdk-openjdk geogebra shellcheck vim firefox syncthing ranger xournalpp ffmpeg obs-studio sxiv arandr man-db brightnessctl unzip unrar python mupdf-gl mediainfo highlight pulseaudio-alsa pulsemixer pamixer  ttf-linux-libertine calcurse xclip noto-fonts-emoji imagemagick thunderbird gimp xorg-setxkbmap wavemon cmus texlive-most dash neofetch htop wireless_tools alsa-utils acpi zip unrar libreoffice nm-connection-editor dunst libnotify dosfstools
+pacman -S --noconfirm xorg-server xorg-xinit xorg-xwininfo xorg-xprop xorg-xbacklight xorg-xdpyinfo xorg-xsetroot picom xbindkeys jdk-openjdk geogebra shellcheck neovim ranger xournalpp ffmpeg obs-studio sxiv arandr man-db brightnessctl unzip unrar python mupdf-gl mediainfo highlight pulseaudio-alsa pulsemixer pamixer  ttf-linux-libertine calcurse xclip noto-fonts-emoji imagemagick thunderbird gimp xorg-setxkbmap wavemon cmus texlive-most dash neofetch htop wireless_tools alsa-utils acpi zip unrar libreoffice nm-connection-editor dunst libnotify dosfstools tlp mpv xorg-xinput cpupower zsh zsh-syntax-highlighting newsboat nomacs pcmanfm openbsd-netcat powertop mupdf-tools
 
 # install aur packages
-sudo -u "$username" yay -S --noconfirm betterlockscreen simple-mtpfs tibasicc-git xflux dashbinsh devour plymouth vim-plug
+sudo -u "$username" paru -S --noconfirm betterlockscreen simple-mtpfs tibasicc-git redshift dashbinsh devour plymouth vim-plug librewolf-bin lf-bin brave-bin
 
 # enable tap to click
 [ ! -f /etc/X11/xorg.conf.d/40-libinput.conf ] && printf 'Section "InputClass"
@@ -80,6 +77,10 @@ sudo -u "$username" yay -S --noconfirm betterlockscreen simple-mtpfs tibasicc-gi
     # Enable left mouse button by tapping
     Option "Tapping" "on"
 EndSection' > /etc/X11/xorg.conf.d/40-libinput.conf
+
+# set global zshenv
+[ ! -d /etc/zsh ] && mkdir -p /etc/zsh
+echo "export ZDOTDIR=\$HOME/.config/zsh" > /etc/zsh/zshenv
 
 # set the real world /etc/sudoers
 sed -i "s/%wheel ALL=(ALL) NOPASSWD: ALL/${username} ALL = NOPASSWD: \/usr\/bin\/mount, \/usr\/bin\/umount/g" /etc/sudoers
